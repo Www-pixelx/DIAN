38d5a1de876a68c27bcb96a0d1857ec4d05dc07d
Www-pixelx <3026425706@qq.com>
2024-09-15 21:16:11 +0800

    Update L1-2 付款.c

--- L1-2 付款.c
+++ L1-2 付款.c
@@ -1,37 +1,25 @@
 #include<stdio.h>
 int main()
 {
-	int sum = 0;
-	int pay = 0;
-	int amount = 0;
-	int i;
+	int sum = 0;//总价
+	int pay = 0;//每次付的钱
+	int amount = 0;//总付款
 	printf("请输入购买商品的总价");
 	scanf("%d",&sum);
 	printf("请付款");
 	while(1){
 	scanf("%d",&pay);
-	int change = pay - sum;
-	if((pay == 1 || pay == 2 || pay == 5) && pay >= sum){
-		printf("成功付款，找您%d元",change);break;
-	}
-	else if((pay == 1 || pay == 2 || pay == 5) && pay < sum)
-	{
-		while(amount < (sum - pay)){
-			printf("金额不足，请继续付款\n");
-			scanf("%d",&i);
-			if(i == 1 || i == 2 || i == 5){
-				amount += i;
-			}
-			else{
-				printf("您只能一次支付一元、二元或五元\n");
-				}
-			}
-		printf("付款成功，找您%d元",amount - sum + pay);break;
-	}
+	amount += pay;
+	if((pay == 1 || pay == 2 || pay == 5) && amount >= sum){
+		printf("成功付款，找您%d元\n",amount - sum);
+		break;}
+	else if((pay == 1 || pay == 2 || pay == 5) && amount < sum){
+	printf("金额不足，剩%d元未付清，请继续付款\n",sum - amount);
+	continue;}		
 	else
 	{
 		printf("您只能一次支付一元、二元或五元，请重新支付。\n");	
 	}
-	}
+}printf("谢谢惠顾！");
 return 0;
 }
