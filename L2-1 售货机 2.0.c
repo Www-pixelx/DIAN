ad5f10eb51e62c9857bc59bf042b0f3313eddf09
Www-pixelx <3026425706@qq.com>
2024-09-20 21:18:18 +0800

    Update L2-1 售货机 2.0.c

--- L2-1 售货机 2.0.c
+++ L2-1 售货机 2.0.c
@@ -8,13 +8,12 @@ int main(){
 	//摆放货物
 	int p[5] = {0};//每个通道的货物数量
 	int mark[5] = {0} ;//空通道标记为 0
-	int n[256] = {0};//每种货物的数量
 	char goods;
 	char g[5];//储存货物的名称
 	int pr[5];//每种货物的单价
 	int pass,qnty,price;
-	while(1){
-	while(1){
+	while(1){//回购的循环
+	while(1){//重新选择货物的循环
 	   printf("请输入摆放货物的名称、通道、单价和数量(用空格隔开)\n");
 	   scanf(" %c %d %d %d",&goods,&pass,&price,&qnty);
 	   while (getchar() != '\n');
@@ -29,18 +28,15 @@ int main(){
 	   }
 	    else {
 	   mark[pass - 1] = 1;//有货标记为 1
-	   p[pass - 1] = qnty;
-	   pr[pass - 1] = price;
-	   g[pass-1] = goods;
-	   int asc = (int)goods;
-	   n[asc] += qnty;
+	   p[pass - 1] = qnty;//增加相应通道的货物数量
+	   pr[pass - 1] = price;//储存货物价格
+	   g[pass-1] = goods;//储存货物名称
 	   printf("%d: %c * %d 单价%d元\n",pass,goods,p[pass-1],price);
 	   char in[4];
 	   printf("是否再次摆放？输入END结束摆放;输入任意值再次摆放。\n");
 	   scanf("%3s",in);
-	   while (getchar() != '\n');
-	   if(strcmp(in,"END") == 0) {
-	   break;}
+	   while (getchar() != '\n');//清空缓存区
+	   if(strcmp(in,"END") == 0) break;//检验输入
 	   }
 	}
 	printf("最终摆放情况:\n");
@@ -51,23 +47,21 @@ int main(){
 	while(1){
 printf("输入购买货物的名称、通道和个数(用空格隔开)\n");
 scanf(" %c %d %d",&goods,&pass,&qnty);
-if (pass < 1 || pass > 5) {printf("通道号必须在1到5之间\n");}
-else if(goods != g[pass-1]) {printf("此通道无该货品\n");}
-else if (qnty > p[pass - 1]) {printf("货物不足\n");}
-else{sum += pr[pass - 1]*qnty;
-    p[pass - 1] -= qnty;
+if (pass < 1 || pass > 5) {printf("通道号必须在1到5之间\n");}//检验通道
+else if(goods != g[pass-1]) {printf("此通道无该货品\n");}//检验货物
+else if (qnty > p[pass - 1]) {printf("货物不足\n");}//检验数量
+else{sum += pr[pass - 1]*qnty;//计算总价
+    p[pass - 1] -= qnty;//减去相应通道的货物数量
 	printf("成功购买\n是否继续购买？结束购买输入END;继续购买输入任意值\n");
 	char input[4];
 	scanf("%3s",input);
 	   while (getchar() != '\n');
-	   if(strcmp(input,"END") == 0) {
-	   break;}
+	   if(strcmp(input,"END") == 0) break;
 }
 }
 int num;//购买的数量	
 int pay;//每次付的钱
 int amount;//总付款
-char i[4];
 printf("总价%d元，请付款",sum);
 	while(1){
 	scanf("%d",&pay);
@@ -84,7 +78,8 @@ printf("总价%d元，请付款",sum);
 		amount -= pay;	
 	    }
 }
-printf("是否再次购买？\n退出购买请输入END；继续购买，输入任意值。");
+printf("是否再次购买？\n退出购买请输入END；继续购买，输入任意值。");//是否回购
+char i[4];
 scanf("%3s",i);
 while (getchar() != '\n');
 if(strcmp(i, "END") == 0)break;
