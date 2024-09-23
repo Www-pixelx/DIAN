1e37a86e663278643ed143efd61a53caf528147e
Www-pixelx <3026425706@qq.com>
2024-09-23 22:57:08 +0800

    Update L2-1 售货机 2.0.c

--- L2-1 售货机 2.0.c
+++ L2-1 售货机 2.0.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+
 int NUMBER[5] = {0}; // 每个通道的货物数量
 char NAME[5][5]; // 储存货物的名称
 int UNIT[5]; // 每种货物的单价
@@ -5,19 +9,23 @@ const int MAX = 50; // 一个通道最多的货物数量
 int sum = 0; // 初始化sum
 
 // 定义函数 货物摆放
-void LAYOUT(char goods, int channel, int qnty, int price) {
+void LAYOUT() {
+	char goods[4];
+	int channel, qnty, price;
+	printf("请依次输入摆放货物的名称、通道、单价和数量(用空格隔开)\n输入END结束摆放\n");
     while (1) { // 重新摆放货物的循环
-        printf("请输入摆放货物的名称、通道、单价和数量(用空格隔开)\n");
-        scanf(" %c %d %d %d", &goods,&channel,&price,&qnty);
+        scanf(" %3s",goods);
+        if (strcmp(goods, "END") == 0) break;
+        scanf(" %d %d %d",&channel, &price, &qnty);
         while (getchar() != '\n'); // 清空缓存区
         int mark;
-        if (channel < 1 || channel > 5) {
+		if (channel < 1 || channel > 5) {
             mark = 1; // 通道输入错误
-        } else if (NUMBER[channel - 1] != 0 && strcmp(NAME[channel - 1], &goods) != 0) {
+        } else if (NUMBER[channel - 1] != 0 && strcmp(NAME[channel - 1], goods) != 0) {
             mark = 2; // 放的货物与通道原有货物不同
-        } else if (NUMBER[channel - 1] != 0 && strcmp(NAME[channel - 1], &goods) == 0 && price != UNIT[channel - 1]) {
+        } else if (NUMBER[channel - 1] != 0 && strcmp(NAME[channel - 1], goods) == 0 && price != UNIT[channel - 1]) {
             mark = 3; // 货物单价输入不同
-        } else if ((NUMBER[channel - 1] != 0 && strcmp(NAME[channel - 1], &goods) == 0 && price == UNIT[channel - 1]) || NUMBER[channel - 1] == 0) {
+        } else if ((NUMBER[channel - 1] != 0 && strcmp(NAME[channel - 1], goods) == 0 && price == UNIT[channel - 1]) || NUMBER[channel - 1] == 0) {
             mark = 4; // 正确摆放
         } else {
             mark = 5; // 有人乱输
@@ -39,13 +47,7 @@ void LAYOUT(char goods, int channel, int qnty, int price) {
                     NUMBER[channel - 1] -= qnty;
                 } else {
                     UNIT[channel - 1] = price;
-                    strcpy(NAME[channel - 1], &goods);
-                    printf("%d: %c * %d 单价%d元\n", channel, goods, NUMBER[channel - 1], price);
-                    char in[4];
-                    printf("是否再次摆放？输入END结束摆放;输入任意值再次摆放。\n");
-                    scanf("%3s", in);
-                    while (getchar() != '\n'); // 清空缓存区
-                    if (strcmp(in, "END") == 0) return;
+                    strcpy(NAME[channel - 1], goods);
                 }
                 break;
             case 5:
@@ -60,15 +62,17 @@ void LAYOUT(char goods, int channel, int qnty, int price) {
 }
 
 // 定义函数 货物购买
-int PURCHASE(char goods, int channel, int qnty, int price) {
+int PURCHASE() {
+	char item;
+	int channel, qnty, price;
     while (1) {
         printf("输入购买货物的名称、通道和个数(用空格隔开)\n");
-        scanf(" %c %d %d", &goods,&channel,&qnty);
+        scanf(" %c %d %d", &item,&channel,&qnty);
         while (getchar() != '\n');
         if (channel < 1 || channel > 5) {
             printf("通道号必须在1到5之间\n");
         } // 检验通道
-        else if (strcmp(NAME[channel - 1], &goods) != 0) {
+        else if (strcmp(NAME[channel - 1], &item) != 0) {
             printf("此通道无该货品\n");
         } // 检验货物
         else if (qnty > NUMBER[channel - 1]) {
@@ -77,7 +81,7 @@ int PURCHASE(char goods, int channel, int qnty, int price) {
         else {
             sum += UNIT[channel - 1] * qnty; // 计算总价
             NUMBER[channel - 1] -= qnty; // 减去相应通道的货物数量
-            printf("成功购买%d通道货物%c共%d个\n是否继续购买？结束购买输入END;继续购买输入任意值\n", channel, goods, qnty);
+            printf("成功购买%d通道货物%c共%d个\n是否继续购买？结束购买输入END;继续购买输入任意值\n", channel, item, qnty);
             char input[4];
             scanf("%3s", input);
             while (getchar() != '\n');
@@ -88,7 +92,8 @@ int PURCHASE(char goods, int channel, int qnty, int price) {
 }
 
 // 定义函数 付款
-void PAYMENT(int num, int pay, int amount) {
+void PAYMENT() {
+	int num, pay, amount = 0;
     printf("总价%d元，请付款\n", sum);
     while (1) {
         scanf("%d", &pay);
@@ -112,12 +117,13 @@ int main() {
     SetConsoleCP(CP_UTF8);
     SetConsoleOutputCP(CP_UTF8);
 
-    char goods;
+    char item;
+    char goods[4];
     int channel, qnty, price, num, pay, amount = 0;
-    LAYOUT(goods, channel, qnty, price);
+    LAYOUT();
     while (1) {
-        sum = PURCHASE(goods, channel, qnty, price);
-        PAYMENT(num, pay, amount);
+        sum = PURCHASE();
+        PAYMENT();
         printf("是否再次购买？\n退出购买请输入END；继续购买，输入任意值。\n"); // 是否回购
         char i[4];
         scanf("%3s", i);
@@ -126,4 +132,4 @@ int main() {
     }
     printf("谢谢惠顾！");
     return 0;
-}
+	}
